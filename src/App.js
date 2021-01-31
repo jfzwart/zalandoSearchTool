2ccc71774b00cdb86ec149de32d7a6553618da4f
Jeroen Zwart <jeroen.zwart@live.com>
2021-01-31 20:16:45 +0100

    fixed small bug on blank search term

--- src/App.js
+++ src/App.js
@@ -1,40 +1,37 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 
-
 const Search = () => {
-  const [term, setTerm] = useState('');
-  const [results, setResults] = useState([]);
+  const [term, setTerm] = useState('Zalando');
+  const [debouncedTerm, setDebouncedTerm] = useState(term);
+  const [results, setResults] = useState([""]);
+
+  useEffect(() => {
+    const timerId = setTimeout(() => {
+      setDebouncedTerm(term);
+    }, 500);
+
+    return () => {
+      clearTimeout(timerId);
+    };
+  }, [term]);
 
   useEffect(() => {
     const search = async () => {
-      const {data} = await axios.get('https://en.wikipedia.org/w/api.php', {
+      const { data } = await axios.get('https://en.wikipedia.org/w/api.php', {
         params: {
           action: 'query',
           list: 'search',
           origin: '*',
           format: 'json',
-          srsearch: term
-        }
+          srsearch: debouncedTerm || " ",
+        },
       });
 
-      setResults(data.query.search); //already prefilter available data
+      setResults(data.query.search);
     };
-
-    if (term && results.length > 0) {
-      search();
-    } else {
-        const timeoutId = setTimeout(() => {
-          if(term) {
-          search();
-          }
-        }, 600);
-
-        return () => {
-          clearTimeout(timeoutId);
-        };
-      }
-    }, [term]);
+    search();
+  }, [debouncedTerm]);
 
   const renderedResults = results.map((result) => {
     return <div key={result.pageid} className="item">
